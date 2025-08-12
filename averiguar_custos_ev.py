0331f1a54dc7f50f0b47b7eabfcdc67363b11769
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2025-08-12 09:08:30 -0300

    novos custos

--- averiguar_custos_ev.py
+++ averiguar_custos_ev.py
@@ -3,7 +3,7 @@ import numpy as np
 from pathlib import Path
 
 # Definindo os caminhos dos arquivos
-csv_path = r"C:\Users\win11\OneDrive\Documentos\Custos Médios\2025\Agosto\ev110825.csv"
+csv_path = r"C:\Users\win11\OneDrive\Documentos\Custos Médios\2025\Agosto\ev120825.csv"
 #"Z:\ANDRIELLY\CONTROLE DE NOTAS.xlsx"
 xlsx_path = r"S:\hor\arquivos\mario\CONTROLE DE NOTAS ATUALIZADO.xlsx"
 output_path = str(Path.home() / "Downloads" / "Averiguar_Custos (EV x NOTA).xlsx")
