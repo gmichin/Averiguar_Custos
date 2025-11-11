04d25aa129d2e0d7b08a5e66aa965898a77dfa00
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2025-11-11 09:14:19 -0300

    novos custos

--- averiguar_custos_mar.py
+++ averiguar_custos_mar.py
@@ -9,7 +9,7 @@ pd.set_option('display.width', 1000)
 
 # Caminhos dos arquivos
 custo_path = r"C:\Users\win11\Downloads\Custos de produtos - Novembro.xlsx"
-margem_path = r"C:\Users\win11\Downloads\MRG_251106 - wapp.xlsx"
+margem_path = r"C:\Users\win11\Downloads\MRG_251110 - waap.xlsx"
 output_path = str(Path.home() / "Downloads" / "Averiguar_Custos (MAR x CUS).xlsx")
 
 def load_data(file_path, file_type):
