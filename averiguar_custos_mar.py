fe3600d22fbd3cc38035363523500839e5e526f5
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2026-01-07 09:11:27 -0300

    retirada de custos não encontrados

--- averiguar_custos_mar.py
+++ averiguar_custos_mar.py
@@ -8,8 +8,8 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
 
 # Caminhos dos arquivos
-custo_path = r"C:\Users\win11\Downloads\Custos de produtos - Dezembro.xlsx"
-margem_path = r"C:\Users\win11\Downloads\MRG_251231 - wapp - v2.xlsx"
+custo_path = r"C:\Users\win11\Downloads\Custos de produtos - Janeiro.xlsx"
+margem_path = r"C:\Users\win11\Downloads\MRG_260106 - wapp.xlsx"
 output_path = str(Path.home() / "Downloads" / "Averiguar_Custos (MAR x CUS).xlsx")
 
 def load_data(file_path, file_type):
