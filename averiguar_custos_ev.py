0362b0e384bed3d848e4ec073a4f4dd4a1861d68
Gustavo Massamichi Nakamura <gmassamichi@gmail.com>
2026-04-15 12:45:13 -0300

    retirada de alguns custos

--- averiguar_custos_ev.py
+++ averiguar_custos_ev.py
@@ -3,7 +3,7 @@ import numpy as np
 from pathlib import Path
 
 # Definindo os caminhos dos arquivos
-csv_path = r"C:\Users\win11\OneDrive\Documentos\Custos Médios\2026\Abril\ev140426.csv"
+csv_path = r"C:\Users\win11\OneDrive\Documentos\Custos Médios\2026\Abril\ev150426.csv"
 xlsx_path = r"S:\hor\arquivos\mario\CONTROLE DE NOTAS ATUALIZADO.xlsx"
 output_path = str(Path.home() / "Downloads" / "Averiguar_Custos (EV x NOTA).xlsx")
 
@@ -44,10 +44,9 @@ produtos_verificar_nao_encontrados = {
     '902': 9.9, '1927': 51.30, '1265': 26.30, '1708': 1.99, '1282': 8.9,
     '3070': 24.65, '1303': 11.9, '925': 9.49, '2989': 7.2, '2082': 4.3,
     '2981': 33, '8599': 22, '3031': 8.97, '6667': 43.5, '865': 9.98,
-    '1039': 4.2, '2087': 26, '2096': 30, '2095': 32, '1584': 7.5, '3065': 8.5,
+    '1039': 4.2, '2087': 26, '2096': 30, '2095': 32, '3065': 8.5, 
     '1753': 6.5, '2094': 29.9, '3003': 41.9, '3004': 7.21, '3002': 29.9,
-    '3097': 2.5, '3098': 4.8, '1669': 7.5, '2056': 5.58, '1781': 9.3,
-    '1501': 11.7, '3060': 45
+    '3097': 2.5, '3098': 4.8, '1669': 7.5, '2056': 5.58, '1501': 11.7,
 }
 
 # Juntando todos os valores de referência
